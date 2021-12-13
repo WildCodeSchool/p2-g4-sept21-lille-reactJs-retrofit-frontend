49d44428b66457d435e96707d66361c36ba74662
Morel Victor <morel.victor1@gmail.com>
2021-12-13 20:13:51 +0100

    Add style for link and global variables for colors

--- src/GlobaleStyle.jsx
+++ src/GlobaleStyle.jsx
@@ -1,14 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --color--primary: rgb(0, 230, 200);
+    --color--secondary: rgb(1, 30, 38);
+    --color--thirdary: #ffffff;
+  }
   *{
     box-sizing: border-box;
+    font-family: 'Josefin Sans', sans-serif;
   }
   body {
     margin: 0;
     padding: 0;
-    background: white;
+    background: var(--color--thirdary);
   }
+  a {
+    text-decoration: none;
+    color:white;
+    margin-left: 20px;
+      :hover {
+    color: var(--color--primary);
+  }
+}
 `;
 
 export default GlobalStyle;
