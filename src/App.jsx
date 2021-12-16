d170f56cb045b1a927e9fd77f82222c57e070fb5
NacymeG <nacyme.gallel@gmail.com>
2021-12-16 17:18:43 +0100

    Deleted header rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from './Components/Header/index';
 import Home from './Pages/Home';
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <Home />
       <Routes>
-        <Route path="/home" element={<Home />}>
+        <Route path="/" element={<Home />}>
           Home
         </Route>
       </Routes>
