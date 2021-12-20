74bbe4361e2045ba752cb890f82df0dab92b89d5
NacymeG <nacyme.gallel@gmail.com>
2021-12-20 17:15:03 +0100

    Deletion of unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+
 import Home from './Pages/Home';
 
 export default function App() {
   return (
     <div>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />}>
           Home
         </Route>
       </Routes>
-      <Footer />
     </div>
   );
 }
