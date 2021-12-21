977ccbd4c3381e4f5ea7f9f55ed862a71c076143
Morel Victor <morel.victor1@gmail.com>
2021-12-21 11:06:16 +0100

    Add route equipe in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/index';
 import Footer from './components/Footer/Footer';
 import Home from './Pages/Home';
+import Team from './Pages/Team/index';
 
 export default function App() {
   return (
@@ -11,8 +12,11 @@ export default function App() {
         <Route path="/home" element={<Home />}>
           Home
         </Route>
+        <Route path="/equipe" element={<Team />}>
+          Home
+        </Route>
       </Routes>
-<Footer />
+      <Footer />
     </div>
   );
 }
