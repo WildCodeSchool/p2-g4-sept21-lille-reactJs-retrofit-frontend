1be949985a0b864dfe289dc0d48952aac0855da0
NacymeG <nacyme.gallel@gmail.com>
2021-12-22 12:14:11 +0100

    Minor correction on app

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ export default function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}>
-          Home
-        </Route>
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </div>
