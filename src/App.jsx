d2a24276df6c23040614a5939271a1bb0eb6993e
NacymeG <nacyme.gallel@gmail.com>
2022-01-05 11:42:10 +0100

    Name folder error (fixed)

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/index';
 import Footer from './Components/Footer/index';
 import SignUpModal from './Components/SignUpModal';
-import SignInModal from './Components/SingInModal';
+import SignInModal from './Components/SignInModal';
 import Home from './Pages/Home';
 
 export default function App() {
