b0e4b5c1194982f48742ba0d16107448fb221bc7
Morel Victor <morel.victor1@gmail.com>
2022-01-06 17:24:31 +0100

    Remove beaty-stars

--- src/Components/ReviewCard/index.jsx
+++ src/Components/ReviewCard/index.jsx
@@ -1,5 +1,3 @@
-import BeautyStars from 'beauty-stars';
-
 import { Card, CardContainer } from './style';
 
 const ReviewCard = ({ ...review }) => {
@@ -7,8 +5,6 @@ const ReviewCard = ({ ...review }) => {
     <>
       <CardContainer>
         <Card>
-          <BeautyStars maxStars={5} value={review.stars} size={34} />
-
           <h3>{review.name} </h3>
           <p>{review.review}</p>
         </Card>
