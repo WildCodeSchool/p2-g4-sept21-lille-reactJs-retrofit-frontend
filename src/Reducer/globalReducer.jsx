f3442f412c5aaae15c9921b01521dca2e2bb80b0
NacymeG <nacyme.gallel@gmail.com>
2022-01-10 11:39:33 +0100

    Deletion of signup cases

--- src/Reducer/globalReducer.jsx
+++ src/Reducer/globalReducer.jsx
@@ -1,5 +1,4 @@
 const initialState = {
-  signUpModal: false,
   signInModal: false,
   isLogged: false,
   token: '',
@@ -13,12 +12,8 @@ export default function globalReducer(state = initialState, action) {
   switch (action.type) {
     case 'CLOSESIGNIN':
       return { ...state, signInModal: false };
-    case 'CLOSESIGNUP':
-      return { ...state, signUpModal: false };
     case 'OPENSIGNIN':
       return { ...state, signInModal: true, signUpModal: false };
-    case 'OPENSIGNUP':
-      return { ...state, signUpModal: true, signInModal: false };
     case 'LOGGEDIN':
       return { ...state, signInModal: false, isLogged: true, ...action.user };
     case 'LOGGOUT':
