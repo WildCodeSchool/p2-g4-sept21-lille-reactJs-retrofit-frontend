9d13dcdfc15d9618e35b5f1e8489b8a4d96aa66f
Morel Victor <morel.victor1@gmail.com>
2022-01-10 15:54:38 +0100

    Add legalnotice folder

---
+++ src/Pages/LegalNotice/index.jsx
@@ -0,0 +1,103 @@
+export default function LegalNotice() {
+  return (
+    <div>
+      <p>
+        Mentions Légales 1.Informations légales : Le site Internet
+        www.everetrofit.fr, ci-après dénommé « EVeRetrofit.com » propose un
+        service de rétrofit (transformation des véhicules thermiques en
+        électriques) aux particuliers et aux entreprises. L&apos;accès au site,
+        sa consultation et son utilisation sont subordonnés à l&apos;acceptation
+        sans réserve des présentes Conditions Générales d&apos;Utilisation de
+        EVeRetrofit.fr. Statut du propriétaire : société Préfixe : SAS Nom de la
+        Société : EVeRetrofit SAS Adresse : HubInnovation, 11, Av. de
+        l&apos;Harmonie, 59650 Villeneuve-d&apos;Ascq Au Capital de : 15 000 €
+        SIRET : XXXXXXXXXXXXXXXX R.C.S. : XXX XXX XXX R.C.S. LILLE Numéro TVA
+        intracommunautaire : FRXXXXXXXXXXXXX Adresse de courrier électronique :
+        contact@everetrofit.com Le Créateur du site est : Développé avec ♥ par
+        les Wilders Le Responsable de la publication est : Anthony Tartare
+        Contactez le responsable de la publication : contact@everetrofit.com Le
+        responsable de la publication est une personne physique Le Webmaster est
+        : Anthony Tartare Contactez le Webmaster : contact@everetrofit.com
+        L’hebergeur du site est : OVH 2 rue Kellermann 59100 Roubaix 2.
+        Description des services fournis : Le site www.everetrofit.fr a pour
+        objet de fournir une information concernant l’ensemble des activités de
+        la société. Le proprietaire du site s’efforce de fournir sur le site
+        www.everetrofit.com des informations aussi précises que possible.
+        Toutefois, il ne pourra être tenue responsable des omissions, des
+        inexactitudes et des carences dans la mise à jour, qu’elles soient de
+        son fait ou du fait des tiers partenaires qui lui fournissent ces
+        informations. Tous les informations proposées sur le site
+        www.everetrofit.com sont données à titre indicatif, sont non
+        exhaustives, et sont susceptibles d’évoluer. Elles sont données sous
+        réserve de modifications ayant été apportées depuis leur mise en ligne.
+        3. Propriété intellectuelle et contrefaçons : Le proprietaire du site
+        est propriétaire des droits de propriété intellectuelle ou détient les
+        droits d’usage sur tous les éléments accessibles sur le site, notamment
+        les textes, images, graphismes, logo, icônes, sons, logiciels… Toute
+        reproduction, représentation, modification, publication, adaptation
+        totale ou partielle des éléments du site, quel que soit le moyen ou le
+        procédé utilisé, est interdite, sauf autorisation écrite préalable à
+        l’email : contact@everetrofit.com Toute exploitation non autorisée du
+        site ou de l’un quelconque de ces éléments qu’il contient sera
+        considérée comme constitutive d’une contrefaçon et poursuivie
+        conformément aux dispositions des articles L.335-2 et suivants du Code
+        de Propriété Intellectuelle. 4. Liens hypertextes et cookies : Le site
+        www.everetrofit.com contient un certain nombre de liens hypertextes vers
+        d’autres sites (partenaires, informations …) mis en place avec
+        l’autorisation de le proprietaire du site. Cependant, le propriétaire du
+        site n’a pas la possibilité de vérifier le contenu des sites ainsi
+        visités et décline donc toute responsabilité de ce fait quand aux
+        risques éventuels de contenus illicites. L’utilisateur est informé que
+        lors de ses visites sur le site www.everetrofit.com, un ou des cookies
+        sont susceptible de s’installer automatiquement sur son ordinateur. Un
+        cookie est un fichier de petite taille, qui ne permet pas
+        l’identification de l’utilisateur, mais qui enregistre des informations
+        relatives à la navigation d’un ordinateur sur un site. Les données ainsi
+        obtenues visent à faciliter la navigation ultérieure sur le site, et ont
+        également vocation à permettre diverses mesures de fréquentation. Le
+        paramétrage du logiciel de navigation permet d’informer de la présence
+        de cookie et éventuellement, de refuser de la manière décrite à
+        l’adresse suivante : www.cnil.fr Le refus d’installation d’un cookie
+        peut entraîner l’impossibilité d’accéder à certains services.
+        L’utilisateur peut toutefois configurer son ordinateur de la manière
+        suivante, pour refuser l’installation des cookies : Sous Internet
+        Explorer : onglet outil / options internet. Cliquez sur Confidentialité
+        et choisissez Bloquer tous les cookies. Validez sur Ok. Sous Netscape :
+        onglet édition / préférences. Cliquez sur Avancées et choisissez
+        Désactiver les cookies. Validez sur Ok. 5. Protection des biens et des
+        personnes – gestion des données personnelles : Utilisateur : Internaute
+        se connectant, utilisant le site susnommé : www.everetrofit.com En
+        France, les données personnelles sont notamment protégées par la loi n°
+        78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L.
+        226-13 du Code pénal et la Directive Européenne du 24 octobre 1995. Sur
+        le site www.everetrofit.com, le proprietaire du site ne collecte des
+        informations personnelles relatives à l’utilisateur que pour le besoin
+        de certains services proposés par le site www.everetrofit.com.
+        L’utilisateur fournit ces informations en toute connaissance de cause,
+        notamment lorsqu’il procède par lui-même à leur saisie. Il est alors
+        précisé à l’utilisateur du site www.everetrofit.com l’obligation ou non
+        de fournir ces informations. Conformément aux dispositions des articles
+        38 et suivants de la loi 78-17 du 6 janvier 1978 relative à
+        l’informatique, aux fichiers et aux libertés, tout utilisateur dispose
+        d’un droit d’accès, de rectification, de suppression et d’opposition aux
+        données personnelles le concernant. Pour l’exercer, adressez votre
+        demande à www.everetrofit.com par email : email du webmaster ou en
+        effectuant sa demande écrite et signée, accompagnée d’une copie du titre
+        d’identité avec signature du titulaire de la pièce, en précisant
+        l’adresse à laquelle la réponse doit être envoyée. Aucune information
+        personnelle de l’utilisateur du site www.everetrofit.com n’est publiée à
+        l’insu de l’utilisateur, échangée, transférée, cédée ou vendue sur un
+        support quelconque à des tiers. Seule l’hypothèse du rachat du site
+        www.everetrofit.com à le proprietaire du site et de ses droits
+        permettrait la transmission des dites informations à l’éventuel
+        acquéreur qui serait à son tour tenu de la même obligation de
+        conservation et de modification des données vis à vis de l’utilisateur
+        du site www.everetrofit.com. Le site www.everetrofit.com est en
+        conformité avec le RGPD voir notre politique RGPD
+        www.everetrofit.com/rgpd/. Les bases de données sont protégées par les
+        dispositions de la loi du 1er juillet 1998 transposant la directive 96/9
+        du 11 mars 1996 relative à la protection juridique des bases de données.
+      </p>
+    </div>
+  );
+}
