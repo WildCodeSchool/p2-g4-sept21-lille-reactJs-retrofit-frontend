5449cd9a407018ff7dbe4b3f7a96b0c0ee92b123
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-13 17:22:31 +0100

    create componant localisation

--- src/Pages/Localisation/index.jsx
+++ src/Pages/Localisation/index.jsx
@@ -1,15 +1,37 @@
+import { React, useEffect, useState } from 'react';
+import axios from 'axios';
 import Map from '../../Components/Map/index';
-import { SContainer, SListAdress, SButtons } from './style';
+import { SContainer, SListAdress, SAdress } from './style';
 
 function Localisation() {
+  const [adressResults, setAdressResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3031/localisation`)
+      .then(({ data }) => {
+        setAdressResults(data);
+      })
+      .catch();
+  }, []);
   return (
     <>
       <SContainer>
         <SListAdress>
-          <SButtons>
-            <p>Nom du garage</p>
-            <input type="text" id="nameGarage" placeholder="Nom du garage" />
-          </SButtons>
+          {adressResults.map((result) => {
+            return (
+              <SAdress>
+                <h3>{[result.name]}</h3>
+                <p>
+                  Adresse: {[result.streetNumber]} {[result.street]}
+                </p>
+                <p>
+                  {[result.postalCode]} {[result.city]}
+                </p>
+                <p>Téléphone: {[result.phone]}</p>
+              </SAdress>
+            );
+          })}
         </SListAdress>
         <Map />
       </SContainer>
