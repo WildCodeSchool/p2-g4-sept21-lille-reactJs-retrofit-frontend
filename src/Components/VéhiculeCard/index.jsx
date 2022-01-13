eef9839a1b1729e77775c5f89ce46c60fb5ed1e1
John-athan14 <guillot.jonathan59@gmail.com>
2022-01-13 14:02:43 +0100

    Add vehicule card

--- src/Components/VéhiculeCard/index.jsx
+++ src/Components/VéhiculeCard/index.jsx
@@ -2,28 +2,29 @@ import PropTypes from 'prop-types';
 // import useState from 'react';
 import { SVéhiculeCard, Btn, Title, Image } from './style';
 
-export default function VéhiculeCard({ name, url }) {
+export default function VéhiculeCard({ model, image, price }) {
   // const [toggle, setToggle] = useState(false);
   // const handleClick = () => {
   //   setToggle(!toggle);
   // };
   return (
     <SVéhiculeCard>
-      <Image src={url} alt={name} />
-      <Title>{name}</Title>
-      <p>A partir de 15 000 €</p>
+      <Image src={image} alt="image voiture" />
+      <Title>{model}</Title>
+      <p>A partir de {price} €</p>
       <Btn>Plus dinfos</Btn>
     </SVéhiculeCard>
   );
 }
-// toggle={toggle} onClick={handleClick}
 
 VéhiculeCard.propTypes = {
-  name: PropTypes.string,
-  url: PropTypes.string,
+  model: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
 };
 
 VéhiculeCard.defaultProps = {
-  name: '',
-  url: '',
+  model: '',
+  image: '',
+  price: 0,
 };
