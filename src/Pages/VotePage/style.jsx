245299157374f1ae825d4fbe7a6140a14d9f2bdd
NacymeG <nacyme.gallel@gmail.com>
2022-01-13 17:28:08 +0100

    VotePage Created

---
+++ src/Pages/VotePage/style.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+// const VoteCardContainer = styled.div`
+//   height: 100%;
+//   width: 100%;
+//   margin-top: 75px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+
+// `;
+
+const VoteContainer = styled.div`
+  margin: 5% 5%;
+  height: 100%;
+  padding: 15px;
+
+  display: flex;
+  flex-direction: column;
+
+  text-align: center;
+
+  h1 {
+    font-size: 40px;
+    margin: 10px;
+  }
+
+  div.voteContainer {
+    display: flex;
+    flex-direction: column;
+    background: var(--color--secondary);
+    color: var(--color--thirdary);
+  }
+
+  div.TopVoteCardWrapper {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  div.VoteCardContainer {
+    margin: 0% 5%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    color: var(--color--thirdary);
+  }
+
+  h3 {
+    color: black;
+    margin: 5% 10%;
+    font-size: 20px;
+  }
+`;
+
+export default VoteContainer;
