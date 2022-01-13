0edb7d332082282517ba2b4e6ecc64499a8a3a63
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-13 17:22:59 +0100

    style  componant localisation

--- src/Pages/Localisation/style.jsx
+++ src/Pages/Localisation/style.jsx
@@ -1,17 +1,39 @@
 import styled from 'styled-components';
 
 export const SContainer = styled.div`
-  margin-top: 75px;
-  padding: 20px;
+  margin-top: 85px;
+  margin-bottom: 10px;
+  padding: 2px;
   display: flex;
   justify-content: center;
 `;
 
 export const SListAdress = styled.div`
-  padding: 20px;
-  border: solid 1px red;
+  padding: 2px;
+  border: solid 1px var(--color--secondary);
   width: 20vw;
+  height: 90vh;
+  overflow: scroll;
 `;
-export const SButtons = styled.div`
-  border: solid 1px yellowgreen;
+export const SAdress = styled.div`
+  border: solid 2px var(--color--primary);
+  margin: 5px;
+  background-color: var(--color--secondary);
+  color: white;
+
+  h3 {
+    text-align: center;
+    padding-top: 3px;
+  }
+  p,
+  h3 {
+    margin: 5px;
+  }
+  p,
+  h3:first-letter {
+    text-transform: capitalize;
+  }
+  display: flex;
+  flex-direction: column;
+  text-transform: lowercase;
 `;
