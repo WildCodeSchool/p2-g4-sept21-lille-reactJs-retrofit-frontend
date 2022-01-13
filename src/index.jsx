01ba6edb0cb276533a75d3ee68829664c91237bc
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-13 14:02:32 +0100

    delete logo garage

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+/* import axios from 'axios'; */
 import App from './App';
 import GlobalStyle from './GlobaleStyle';
 import globalReducer from './Reducer/globalReducer';
 
 const store = createStore(globalReducer);
+/* axios.defaults.baseURL = process.env.REACT_APP_BASE_URL; */
 
 ReactDOM.render(
   <React.StrictMode>
