bdb6cb07d5d65c873477fb38fb214000df37c942
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-17 16:56:08 +0100

    Responsive list adress

--- src/Pages/Localisation/style.jsx
+++ src/Pages/Localisation/style.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Telephone } from '@styled-icons/bootstrap/Telephone';
+import { LocationPin } from '@styled-icons/entypo/LocationPin';
 
 export const SContainer = styled.div`
   margin-top: 85px;
@@ -6,6 +8,12 @@ export const SContainer = styled.div`
   padding: 2px;
   display: flex;
   justify-content: center;
+
+  @media only screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column-reverse;
+    margin: 160px 20px 20px 20px;
+  }
 `;
 
 export const SListAdress = styled.div`
@@ -14,7 +22,20 @@ export const SListAdress = styled.div`
   width: 20vw;
   height: 90vh;
   overflow: scroll;
+  ::-webkit-scrollbar {
+    width: 3px;
+  }
+  overflow-x: hidden;
+  @media only screen and (max-width: 768px) {
+    margin-top: 5px;
+
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+  }
 `;
+
 export const SAdress = styled.div`
   border: solid 2px var(--color--primary);
   margin: 5px;
@@ -35,5 +56,16 @@ export const SAdress = styled.div`
   }
   display: flex;
   flex-direction: column;
-  text-transform: lowercase;
+`;
+export const PhoneWhite = styled(Telephone)`
+  color: white;
+  height: 15px;
+  width: 15px;
+  margin-right: 3px;
+  margin-left: 5px;
+`;
+export const AdressWhite = styled(LocationPin)`
+  color: white;
+  height: 20px;
+  width: 20px;
 `;
