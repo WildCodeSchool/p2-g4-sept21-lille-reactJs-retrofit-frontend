4f991344f39f86ad6825c57c706eb5c827333061
NacymeG <nacyme.gallel@gmail.com>
2022-01-25 14:30:59 +0100

    rating stars added

--- src/Components/ReviewCard/index.jsx
+++ src/Components/ReviewCard/index.jsx
@@ -1,3 +1,4 @@
+import Rating from '@mui/material/Rating';
 import { Card, CardContainer } from './style';
 
 const ReviewCard = ({ ...review }) => {
@@ -5,6 +6,8 @@ const ReviewCard = ({ ...review }) => {
     <>
       <CardContainer>
         <Card>
+          <Rating name="read-only" value={review.stars} size="large" readOnly />
+
           <h3>{review.name} </h3>
           <p>{review.review}</p>
         </Card>
