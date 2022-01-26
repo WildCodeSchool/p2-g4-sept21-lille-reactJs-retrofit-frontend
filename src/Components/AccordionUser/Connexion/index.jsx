0d68c30cfc67babe00cbf774f483427da3831d81
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-26 11:33:13 +0100

    create micro-component connexion

--- src/Components/AccordionUser/Connexion/index.jsx
+++ src/Components/AccordionUser/Connexion/index.jsx
@@ -1,9 +1,27 @@
-import { Content } from './style';
+import { Content, Row, Row2, Button } from './style';
 
 export default function Connexion() {
   return (
     <>
-      <Content>{`<3`}</Content>
+      <Content>
+        <Row>
+          <p>Nouvelle adresse e-mail :</p>
+          <input type="email" />
+          <p>Resaisir votre adresse e-mail :</p>
+          <input type="email" />
+          <Button>
+            <input type="submit" value="Modifier" />
+          </Button>
+        </Row>
+        <Row2>
+          <p>Ancien mot de passe:</p>
+          <input type="search" />
+          <p>Nouveau mot de passe:</p>
+          <input type="search" />
+          <p>Confirmation du mot de passe:</p>
+          <input type="search" />
+        </Row2>
+      </Content>
     </>
   );
 }
