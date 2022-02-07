767bd33fefca19bf93ea75dffd03c3f981ab1e94
Morel Victor <morel.victor1@gmail.com>
2022-02-07 10:56:10 +0100

    New css for vehicule page and add responsive

--- src/Components/VehiculeCard/index.jsx
+++ src/Components/VehiculeCard/index.jsx
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { SVehiculeCard, Btn, Image, Text } from './style';
 
-export default function VehiculeCard({ model, image, price }) {
+export default function VehiculeCard({ model, image, price, brand }) {
   return (
     <SVehiculeCard>
+      <Image src={image} alt="image voiture" />
+      <Text>
+        <h2>{brand}</h2>
+        <h2>{model}</h2>
+        <p>A partir de {price} €</p>
+      </Text>
       <Link to={`/Vehicules/${model}`}>
-        <Image src={image} alt="image voiture" />
-        <Text>
-          <h2>{model}</h2>
-          <p>A partir de {price} €</p>
-        </Text>
         <Btn>Configurer</Btn>
       </Link>
     </SVehiculeCard>
@@ -19,11 +20,13 @@ export default function VehiculeCard({ model, image, price }) {
 
 VehiculeCard.propTypes = {
   model: PropTypes.string,
+  brand: PropTypes.string,
   image: PropTypes.string,
   price: PropTypes.number,
 };
 
 VehiculeCard.defaultProps = {
+  brand: '',
   model: '',
   image: '',
   price: 0,
--- src/Pages/Cars/index.jsx
+++ src/Pages/Cars/index.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import VehiculeCard from '../../Components/VehiculeCard';
-
-import SCarsView from './style';
+import { SCarsView, Title } from './style';
 
 function Vehicules() {
   const [cars, setCars] = useState([]);
@@ -13,17 +12,21 @@ function Vehicules() {
     });
   }, []);
   return (
-    <SCarsView>
-      {cars.map((data) => {
-        return (
-          <VehiculeCard
-            model={data.model}
-            image={data.image}
-            price={data.price}
-          />
-        );
-      })}
-    </SCarsView>
+    <div>
+      <Title>Choisissez votre prochain véhicule à rétrofité</Title>
+      <SCarsView>
+        {cars.map((data) => {
+          return (
+            <VehiculeCard
+              brand={data.brand}
+              model={data.model}
+              image={data.image}
+              price={data.price}
+            />
+          );
+        })}
+      </SCarsView>
+    </div>
   );
 }
 
