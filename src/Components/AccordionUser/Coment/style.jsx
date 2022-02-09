f591842b2bcb677691e9ffe0ac45e14acc77194b
Caroline COPY-DENHEZ <caroline.copy19@gmail.com>
2022-02-09 10:11:43 +0100

    Fix css for Comment and Question components

--- src/Components/AccordionUser/Coment/style.jsx
+++ src/Components/AccordionUser/Coment/style.jsx
@@ -20,17 +20,29 @@ export const BorderContainer = styled.div`
     padding-left: 1vw;
     font-size: 18px;
   }
+  @media (max-width: 768px) {
+    max-width: 80vw;
+    height: 215px;
+    padding-left: 5px;
+  }
 `;
 
 export const Content = styled.div`
-  background-color: rgb(1, 30, 38);
+  background-color: var(--color--secondary);
   padding-bottom: 0.6rem;
   color: white;
   border-radius: 10px;
-  max-width: 70vw;
+  max-width: 80vw;
   height: 30vh;
   padding: 5px 0 5px 0;
   display: flex;
   align-items: center;
   flex-direction: column;
+  @media (max-width: 768px) {
+    max-width: 80vw;
+    height: 45vh;
+    padding-bottom: 6px;
+    display: flex;
+    justify-content: space-between;
+  }
 `;
